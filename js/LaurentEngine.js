f621b1ef20f9439274119b216bce514d5f3645df
Laurent B <laurent.brival@ixxitechside.com>
2018-02-09 15:05:27 +0100

    Ajout Engine Laurent

---
+++ js/LaurentEngine.js
@@ -0,0 +1,21 @@
+var LaurentEngine = function () {
+    var self = {};
+    var internalGrid = {};
+    var gridIsComplete = false;
+
+    function updateInternalGrid(){
+
+    }
+
+    self.getNextMove = function(neighboringCells){
+        return 'up';
+    };
+
+    self.isMapComplete = function (){
+        var isMapComplete = false;
+
+        return isMapComplete;
+    };
+
+    return self;
+};
