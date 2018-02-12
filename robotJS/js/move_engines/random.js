45509eb657950f1aeb431f823efbd104fc522d35
NicoWip <synper_1999@yahoo.fr>
2018-02-12 20:59:21 +0100

    random engine :D - js level 2

--- robotJS/js/move_engines/random.js
+++ robotJS/js/move_engines/random.js
@@ -2,24 +2,9 @@ var random = function () {
     var self = {};
     var internalGrid = {};
     var gridIsComplete = false;
-
-    //var DirectionEnum = Object.freeze({1:"up", 2:"left", 3:"down", 4:"right"})
-    /*var DirectionEnum = (function() {
-        function ErrorValue(value, friendly) {
-          this.value = value;
-          this.friendly = friendly;
-        }
-        ErrorValue.prototype = {
-          toString: function() { return this.friendly; },
-          valueOf: function() { return this.value; }
-        };
-        return {
-          'up': new ErrorValue(1, 'up'),
-          'left': new ErrorValue(2, 'left'),
-          'down': new ErrorValue(3, 'down'),
-          'right': new ErrorValue(4, 'right'),
-        };
-    })();*/
+    var countMove = 0;
+    var maxMove = 300;
+    
     var direction = ['up', 'right', 'bottom', 'left'];
     
     function updateInternalGrid(){
@@ -33,12 +18,14 @@ var random = function () {
 
     self.getNextMove = function(neighboringCells){
         var aleat = entierAleatoire(0,3);
+        countMove++;
         return direction[aleat];
     };
 
     self.isMapComplete = function (){
         var isMapComplete = false;
-
+        if (countMove === maxMove)
+            isMapComplete = true;
         return isMapComplete;
     };
 
